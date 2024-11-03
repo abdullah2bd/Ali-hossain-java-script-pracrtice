17a3318c927fc2b1c61f8801be6d88ff38b8c36f
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-11-03 07:39:53 +0600

    let

---
+++ Variable/variable.js
@@ -0,0 +1,64 @@
+let sum1 = 100;
+let sum2 = 134;
+let sum3 = 100;
+let sum4 = 134;
+let sum5 = 100;
+let sum6 = 134;
+let sum7 = 100;
+let sum8 = 134;
+let sum9 = 100;
+let sum10 = 134;
+let sum11= 100;
+let sum12= 134;
+let sum13 = 100;
+let sum14 = 134;
+let sum15 = 100;
+let sum16 = 134;
+let sum17 = 100;
+let sum18 = 134;
+let sum19 = 100;
+// let sum20 = 134;
+
+
+// let total = sum1 + sum2 + sum3 + sum4 + sum5 + sum6 + sum7 + sum8 + sum9;
+
+
+let total = [
+  sum1, sum2, sum3
+]
+console.log(total);
+
+
+
+
+let totals = {
+  sum20: 134,
+  sum16: 134,
+  sum17: 100,
+  sum18: 134,
+  sum19: 100,
+}
+
+console.log(totals);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
