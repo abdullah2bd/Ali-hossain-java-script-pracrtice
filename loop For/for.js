0f38f8d68964e5c3c44256c8da3d863c8e77140f
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-11-03 20:57:50 +0600

    for loop complete

---
+++ loop For/for.js
@@ -0,0 +1,46 @@
+let student = ["Jubaiar", "Habib", "Siam", "jamal", "Kamal"];
+
+for (let stu = 0; stu < student.length; stu++) {
+  console.log(4 + "Text");
+}
+
+// console.log(student);
+
+let love = ["I Love Her"];
+
+for (i = 0; i < 6; i++) {
+  console.log(love);
+}
+
+let nam = {
+  Name: "Alah",
+  Name: "Allah",
+};
+for (let nem = 0; nem < 7; nem++) {
+  console.log(nam);
+}
+
+let like = [
+  "I love Allah",
+  "I love Mohammad sm.",
+  "I love my mother",
+  "I love my father",
+];
+
+for (let num = 0; num < 10; num++) {
+  console.log(like);
+}
+
+let nAm = {
+  Love1: "I love Allah",
+  Love2: "I love Mohammad sm.",
+  Love3: "I love my mother",
+  Love4: "I love my father",
+};
+
+for (let lvlist = 0; lvlist < 11; lvlist++) {
+  console.log(nAm);
+}
+
+
+
