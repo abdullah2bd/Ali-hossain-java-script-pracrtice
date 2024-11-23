157e8f6f312a45740f33518e181b32a44c8fa4fb
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-11-23 21:59:00 +0600

    js find

---
+++ find and reduce/find.js
@@ -0,0 +1,24 @@
+// const array = [5, 12, 8, 130, 44];
+
+// const finds = array.find((element) => element > 12);
+
+// console.log(finds);
+
+// const arrays = [33, 46, 25, 84, 19, 62, 48, 3, 2, 65, 73, 84, 99, 90];
+// const founds = arrays.find(function(item){
+//   return item > 60;
+// })
+// console.log(founds);
+
+const arrays = [33, 46, 25, 84, 19, 62, 48, 3, 2, 65, 73, 84, 99, 90];
+const founds = arrays.find((item) => item > 60 );
+ console.log(founds);
+
+
+
+
+
+
+
+ 
+
