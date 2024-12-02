e9c2423a6a8dda535e73f2a682aeec8603690994
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-12-02 09:30:40 +0600

    add to cart

---
+++ addd to cart/js/homew.js
@@ -0,0 +1,64 @@
+const products = [
+  {
+    id: 1,
+    title: "First Product",
+    price: 99,
+  },
+  {
+    id: 2,
+    title: "Second Product",
+    price: 49,
+  },
+  {
+    id: 3,
+    title: "Third Product",
+    price: 35,
+  },
+  {
+    id: 4,
+    title: "Product Title",
+    price: 56,
+  },
+  {
+    id: 5,
+    title: "Product Title",
+    price: 73,
+  },
+  {
+    id: 6,
+    title: "Product Title",
+    price: 83,
+  },
+  {
+    id: 7,
+    title: "Product Title",
+    price: 45,
+  },
+  {
+    id: 8,
+    title: "Product Title",
+    price: 2,
+  },
+  {
+    id: 9,
+    title: "Product Title",
+    price: 2,
+  },
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
